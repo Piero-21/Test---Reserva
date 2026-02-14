c63c46bce49f49ebb7c9d89c9c0a9d0ec058239c
Piero Alessandro Yataco Armellon <piero.sistemas21@gmail.com>
2026-02-13 19:03:48 -0500

    12

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,24 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
+
   return {
+    // 👇 ESTA ES LA LINEA CLAVE
+    base: "/Test---Reserva/",
+
     server: {
       port: 3000,
       host: "0.0.0.0",
     },
+
     plugins: [react()],
+
     define: {
       "import.meta.env.VITE_GEMINI_API_KEY": JSON.stringify(
         env.VITE_GEMINI_API_KEY,
       ),
     },
+
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
